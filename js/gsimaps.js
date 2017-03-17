d099dab60a8630f8e877832117ab6583ac3e0484
Daisuke Yoshida <nro2dai@gmail.com>
2017-03-17 12:11:28 +0900

    タグ情報画面の修正

--- js/gsimaps.js
+++ js/gsimaps.js
@@ -7133,6 +7133,9 @@ GSI.SakuzuDialog = GSI.Dialog.extend( {
 		var tr = $( '<tr>' );
 		tr.append( $( '<td>' ).css( {'white-space':'nowrap'} ).html( 'アイコン:' ) );
 		tr.append( $( '<td>' ).append( this._pointIconImage ) );
+
+		tr.append( $( '<td>' ).css( {'white-space':'nowrap'} ).html( '&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://bitbucket.org/nro2dai/cerd-ar/wiki/GeoJSON" target="_blank">データの詳細について</a>' ) );
+
 /*
 		tr.append( $( '<td>' ).css( {'white-space':'nowrap'} ).html( '&nbsp;&nbsp;拡大率:' ) );
 
@@ -7311,6 +7314,52 @@ GSI.SakuzuDialog = GSI.Dialog.extend( {
 		//新規追加した行（tr）を，項目行の下に移動
 		$('#tbl_check tr:last').insertBefore($('#tbl_check tr').eq(0).next());
 
+		/*
+		//災害タグの場合
+		if (selectedIconName == '068.png' || selectedIconName == '1102.png' || selectedIconName == '1105.png' || selectedIconName == '1106.png' ){
+
+			this._initEditInfo( this._editingTarget.getEditingInfo() );
+
+			var tr = this._createEditInfoTableLine(0, "icon", selectedIconName);
+			this._infoTableTbody.append( tr );
+
+			var tr = this._createEditInfoTableLine(1, "id", "");
+			this._infoTableTbody.append( tr );
+
+			var tr = this._createEditInfoTableLine(2, "name", "");
+			this._infoTableTbody.append( tr );
+
+			var tr = this._createEditInfoTableLine(3, "description", "");
+			this._infoTableTbody.append( tr );
+
+			var tr = this._createEditInfoTableLine(4, "info_type", "warn");
+			this._infoTableTbody.append( tr );
+
+			var tr = this._createEditInfoTableLine(5, "risk_type", "");
+			this._infoTableTbody.append( tr );
+
+			var tr = this._createEditInfoTableLine(6, "start", "");
+			this._infoTableTbody.append( tr );
+
+			var tr = this._createEditInfoTableLine(7, "stop", "");
+			this._infoTableTbody.append( tr );
+
+			var tr = this._createEditInfoTableLine(8, "range", "");
+			this._infoTableTbody.append( tr );
+
+			var tr = this._createEditInfoTableLine(9, "message1", "");
+			this._infoTableTbody.append( tr );
+
+			var tr = this._createEditInfoTableLine(10, "message2", "");
+			this._infoTableTbody.append( tr );
+
+		//情報タグの場合
+		} else {
+
+			this._initEditInfo( this._editingTarget.getEditingInfo() );
+
+		}
+*/
 
 	},
 	_refreshEditingIconHTML : function( html )
@@ -7851,7 +7900,7 @@ GSI.SakuzuDialog = GSI.Dialog.extend( {
 		td = $( '<td>' ).css( { 'width' : '30%', 'vertical-align':'middle' } );
 
 		var nameTextarea = $( '<textarea>' ).addClass( 'inputtextarea')
-			.attr( { name: 'info_table_key', 'placeholder':'(例: 災害の種別)', rows:1} )
+			.attr( { name: 'info_table_key', 'placeholder':'(項目名)', rows:1} )
 			.css( { 'display':'block','width' : '100%' } ).val( key ? key : '' );
 		td.append( nameTextarea );
 		tr.append( td );
@@ -7859,7 +7908,7 @@ GSI.SakuzuDialog = GSI.Dialog.extend( {
 
 		var valueTextarea = $( '<textarea>' ).addClass( 'inputtextarea')
 			.css({'display':'block',width:"100%"})
-			.attr( { name: 'info_table_value', 'placeholder':'(例: 20m)',rows:1} ).val( value ? value : '' );
+			.attr( { name: 'info_table_value', 'placeholder':'(値を入力してください)',rows:1} ).val( value ? value : '' );
 		td.append( valueTextarea );
 		tr.append( td );
 
@@ -7957,8 +8006,8 @@ GSI.SakuzuDialog = GSI.Dialog.extend( {
 		else
 			this._infoFreeWordTextarea.val( '' );
 		 */
-		
-		// アイコンのタイトル（name/Name）を非表示 
+
+		// アイコンのタイトル（name/Name）を非表示
 		document.getElementById('head').style.display='none';
 		this._infoTableTbody.empty();
 
@@ -7995,15 +8044,24 @@ GSI.SakuzuDialog = GSI.Dialog.extend( {
 				selectedIconName =selectedIconName.replace('"','');
 			}
 
-
+		//タグ情報の新規追加画面
 		if ( !info || !info.table || info.table.length <= 0 )
 		{
-			//アイコン選択した後に，値を挿入する
 			var tr = this._createEditInfoTableLine(0, "icon", selectedIconName);
 			this._infoTableTbody.append( tr );
 
-			var tr = this._createEditInfoTableLine(1, "", "");
+			var tr = this._createEditInfoTableLine(1, "id", "");
 			this._infoTableTbody.append( tr );
+
+			var tr = this._createEditInfoTableLine(2, "name", "");
+			this._infoTableTbody.append( tr );
+
+			var tr = this._createEditInfoTableLine(3, "description", "");
+			this._infoTableTbody.append( tr );
+
+			var tr = this._createEditInfoTableLine(4, "info_type", "info");
+			this._infoTableTbody.append( tr );
+
 			return;
 		}
 
